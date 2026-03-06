254192316359e7bc0ce98b70db3c648022a4fb64
Sieunhanom15 <87540541+Romet0123@users.noreply.github.com>
2026-03-06 19:57:44 +0700

    Setup Robot & Animation

---
+++ GDS5_A1/Assets/ExplosiveLLC/Fighter Pack Bundle FREE/Demo Elements/Code/CameraControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:17a995476a2d99d04dbabedf7acff20b4a93f8f61a46aeb06258c824305c2edc
+size 4220
---
+++ GDS5_A1/Assets/ExplosiveLLC/Fighter Pack Bundle FREE/Demo Elements/Code/FighterAnimationDemoFREE.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24056c6d0fa82033f16f0f7a00b6122c4a70f0b2d0492b386c5bd03b66d1b3b3
+size 1315
