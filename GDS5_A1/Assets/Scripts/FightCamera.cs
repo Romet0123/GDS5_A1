d25b84cad43d660d273218663ff3eefa7441467b
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-07 20:40:51 +0700

    Camera
    Automatically Camera Locking Towards Opponent and Third Person Camera Perspective

---
+++ GDS5_A1/Assets/Scripts/FightCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6448549773d5a6aa0f34994477b60b0c8d46e24f1e6e13e39b1e0c827d0f30c7
+size 980
--- GDS5_A1/Assets/Scripts/VoiceController.cs
+++ GDS5_A1/Assets/Scripts/VoiceController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28f03e2f13a77bdbe241352355ddc5822db20022e7fb517b2c8d2d4a9e26d8dc
-size 2919
+oid sha256:89f5d0aa0116da622e28b792122b43588f2a558d5b8d0ce39310c0cd7d3b9724
+size 6815
