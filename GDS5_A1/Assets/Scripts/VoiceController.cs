ddefcc0cb6d9440df85e10d98855e037c4d41e44
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-08 22:55:54 +0700

    Jab animation added
    After command jab it will automatically go back to ready

--- GDS5_A1/Assets/Scripts/VoiceController.cs
+++ GDS5_A1/Assets/Scripts/VoiceController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bb3e85c86e4eb04e7167d8ebe77dc38d23d0c253f89ce59c7d292597c1a9b41
-size 7935
+oid sha256:ebddc0c134000c8b7ad2dea5fe5559397e70533379d948f0ab130e3280c78cac
+size 8481
