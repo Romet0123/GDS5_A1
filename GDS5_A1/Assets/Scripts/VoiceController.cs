8eb1707fc264714b83c90c0c5c3ae6f75afac1de
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-10 11:19:16 +0700

    Add left hook animation
    Change some command and bug fixes for jab animation and cross animation

--- GDS5_A1/Assets/Scripts/VoiceController.cs
+++ GDS5_A1/Assets/Scripts/VoiceController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a7cbb1b23c66527e1fbc8155a0eb32750c94df2fff103875292f05948be6506
-size 10780
+oid sha256:2f17b8c5cb0a0d01d77215d097606b40099325f93fae6fc4cf77e267f1eacbe3
+size 11553
