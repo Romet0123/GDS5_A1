376761083bbef298f274e3802c2be34a4d16bb86
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-11 10:50:41 +0700

    Push in progress of creating robot gameplay
    Hitbox for lefthand

---
+++ GDS5_A1/Assets/Scripts/Hitbox.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3d219b96db98f9f5bb84655b587f6e502f78a138f64f865f4aef83efdda4517
+size 1337
---
+++ GDS5_A1/Assets/Scripts/RobotCombat.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df6cbe9fa78d99f3606db702dc6f82d3fe2b383fffe4136dc0f1a0fc1edc52b3
+size 867
