bdfda727eafc6ac935e8202b2c32586a37d7d48b
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-17 13:59:01 +0700

    Redo camera work and now opponent will stop at a certain distance

--- GDS5_A1/Assets/Scripts/Opponent.cs
+++ GDS5_A1/Assets/Scripts/Opponent.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df208db9aea4a6dac06e5cb1d1db49a9c733313b27f27487d2dda9422076aea3
-size 5062
+oid sha256:44a9d59e2a963b79ca72d8816b0c592dae6a45e684db5f91624892f2084de30e
+size 5427
