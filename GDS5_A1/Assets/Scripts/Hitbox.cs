ae0c5f841fae42ab60d153463eff7a1ab5a3a695
nhatmn1712 <s3930338@rmit.edu.vn>
2026-03-20 15:06:01 +0700

    Increase damage of upper cut (20).
    20/2/2026

--- GDS5_A1/Assets/Scripts/Hitbox.cs
+++ GDS5_A1/Assets/Scripts/Hitbox.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7520d1e6f66827519a53254514e67ea62e2ce589f90a5b6bd60898bde178ac58
-size 4391
+oid sha256:d95d9179b50d3fabbc0c8a4cb8ba7975193f9f5e272b4819fe61df177abdc105
+size 4974
