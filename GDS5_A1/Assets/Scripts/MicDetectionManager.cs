a6eab5a3f01a167d1cbbb07df5ea981e67b44fc3
Romet0123 <nguyenphathuy0123@gmail.com>
2026-03-25 20:55:32 +0700

    Balance Gameplay

--- GDS5_A1/Assets/Scripts/MicDetectionManager.cs
+++ GDS5_A1/Assets/Scripts/MicDetectionManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:548715b6a5c58a296bedd63cfd77578c2d2d6d89b3c5fb2768a2fba9f3b40535
-size 5159
+oid sha256:a2af76c056562b6004e1f60bc5d6d08ebd58ebb89039bf2720c75d45d0fa34ea
+size 6029
