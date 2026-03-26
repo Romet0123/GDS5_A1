1ce41cc51e0a4109bb102e56ee90ce04bd91ab7f
Romet0123 <nguyenphathuy0123@gmail.com>
2026-03-26 20:50:05 +0700

    Fix Text and Initial Audio Mixer

--- GDS5_A1/Assets/Scripts/UIPauseManager.cs
+++ GDS5_A1/Assets/Scripts/UIPauseManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8872ca0db03af47745e463ae4c0057347fffd2ca13fc1bfd2870c61de8debf98
-size 7473
+oid sha256:cb2b9d3ab7fb99cd7f85d4ce88c516770b49c434d6123bada42e348058f77ff5
+size 7925
